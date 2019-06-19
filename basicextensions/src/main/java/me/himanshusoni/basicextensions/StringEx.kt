e8413744711ced854f3a41b53b62c9e18218e040
Himanshu Soni <himanshusoni.me@gmail.com>
2019-06-19 18:22:21 +0530

    String extension updated

--- basicextensions/src/main/java/me/himanshusoni/basicextensions/StringEx.kt
+++ basicextensions/src/main/java/me/himanshusoni/basicextensions/StringEx.kt
@@ -1,5 +1,8 @@
 package me.himanshusoni.basicextensions
 
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
+
 fun String.mask(visibleFirst: Int, visibleLast: Int): String {
     var output = ""
     for (i in 0 until this.length) {
@@ -10,4 +13,33 @@ fun String.mask(visibleFirst: Int, visibleLast: Int): String {
         }
     }
     return output
+}
+
+
+fun String.highlight(query: String?, highlightColor: Int): Spannable {
+    query?.let {
+        if (!it.isBlank() && this.toLowerCase().contains(it.toLowerCase())) {
+            var startPos = this.toLowerCase().indexOf(it.toLowerCase(), 0)
+            val spanText = Spannable.Factory.getInstance().newSpannable(this)
+
+            var hasMore: Boolean
+            do {
+                val endPos = startPos + it.length
+                spanText.setSpan(
+                    ForegroundColorSpan(highlightColor),
+                    startPos,
+                    endPos,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                startPos = this.toLowerCase().indexOf(it.toLowerCase(), endPos)
+                hasMore = startPos != -1
+            } while (hasMore)
+
+            return spanText
+        } else {
+            return Spannable.Factory.getInstance().newSpannable(this)
+        }
+    } ?: run {
+        return Spannable.Factory.getInstance().newSpannable(this)
+    }
 }
