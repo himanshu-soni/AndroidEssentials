323f58e7c718b070b86c7db3a56b728c57c35212
Himanshu Soni <himanshusoni.me@gmail.com>
2019-06-19 10:26:37 +0530

    notify added in adapter

--- recyclerview/src/main/java/me/himanshusoni/androidessentials/recyclerview/adapter/BaseRecyclerViewAdapter.kt
+++ recyclerview/src/main/java/me/himanshusoni/androidessentials/recyclerview/adapter/BaseRecyclerViewAdapter.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import java.util.*
 
+
 abstract class BaseRecyclerViewAdapter<T, VH : RecyclerView.ViewHolder>(var dataSet: ArrayList<T>) :
-        RecyclerView.Adapter<VH>() {
+    RecyclerView.Adapter<VH>() {
 
     fun getItem(position: Int): T {
         return dataSet[position]
@@ -15,46 +16,46 @@ abstract class BaseRecyclerViewAdapter<T, VH : RecyclerView.ViewHolder>(var data
         return dataSet.size
     }
 
-    fun add(item: T) {
+    fun add(item: T, notify: Boolean = true) {
         val positionStart = dataSet.size
         dataSet.add(item)
-        notifyItemInserted(positionStart)
+        if (notify) notifyItemInserted(positionStart)
     }
 
-    fun add(position: Int, item: T) {
+    fun add(position: Int, item: T, notify: Boolean = true) {
         dataSet.add(position, item)
-        notifyItemInserted(position)
+        if (notify) notifyItemInserted(position)
     }
 
-    fun add(list: Collection<T>) {
+    fun add(list: Collection<T>, notify: Boolean = true) {
         val startPosition = dataSet.size
         dataSet.addAll(list)
-        notifyItemRangeInserted(startPosition, list.size)
+        if (notify) notifyItemRangeInserted(startPosition, list.size)
     }
 
-    fun remove(item: T) {
+    fun remove(item: T, notify: Boolean = true) {
         val position = dataSet.indexOf(item)
-        if (position >= 0) removeAt(position)
+        if (position >= 0) removeAt(position, notify)
     }
 
-    fun removeAt(position: Int) {
+    fun removeAt(position: Int, notify: Boolean = true) {
         dataSet.removeAt(position)
-        notifyItemRemoved(position)
+        if (notify) notifyItemRemoved(position)
     }
 
-    fun clear() {
+    fun clear(notify: Boolean = true) {
         val size = itemCount
         dataSet.clear()
-        notifyItemRangeRemoved(0, size)
+        if (notify) notifyItemRangeRemoved(0, size)
     }
 
-    fun removeAll() {
+    fun removeAll(notify: Boolean = true) {
         val size = dataSet.size
         if (size > 0) {
             for (i in 0 until size) {
                 dataSet.removeAt(0)
             }
-            notifyItemRangeRemoved(0, size)
+            if (notify) notifyItemRangeRemoved(0, size)
         }
     }
 
@@ -68,7 +69,10 @@ abstract class BaseRecyclerViewAdapter<T, VH : RecyclerView.ViewHolder>(var data
         if (savedInstanceState == null || !savedInstanceState.containsKey("dataSet")) {
             return
         }
+        @Suppress("UNCHECKED_CAST")
         dataSet = savedInstanceState.getSerializable("dataSet") as ArrayList<T>
         notifyDataSetChanged()
     }
+
+
 }
