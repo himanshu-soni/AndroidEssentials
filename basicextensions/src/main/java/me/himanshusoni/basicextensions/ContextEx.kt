a860b8d0f4d02acb7e2a03ae32af973279cc1500
Himanshu Soni <himanshusoni.me@gmail.com>
2019-07-01 16:37:29 +0530

    share/send intent extensions added

--- basicextensions/src/main/java/me/himanshusoni/basicextensions/ContextEx.kt
+++ basicextensions/src/main/java/me/himanshusoni/basicextensions/ContextEx.kt
@@ -1,10 +1,14 @@
 package me.himanshusoni.basicextensions
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
+import android.net.Uri
 import android.support.annotation.StringRes
 import android.widget.Toast
+import java.io.File
+
 
 fun Context.toast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 fun Context.toast(@StringRes msgRes: Int) = toast(getString(msgRes))
@@ -16,3 +20,25 @@ fun Context.isConnectedToNetwork(): Boolean {
 }
 
 fun Context.isCameraAvailable(): Boolean = packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+
+fun Context.shareFile(file: File, header: String = "Share") {
+    val uri = Uri.fromFile(file)
+    shareUri(uri, header)
+}
+
+fun Context.shareUri(uri: Uri, header: String = "Share") {
+    val sharingIntent = Intent(Intent.ACTION_SEND)
+    sharingIntent.type = "*/*"
+    sharingIntent.putExtra(Intent.EXTRA_STREAM, uri)
+    startActivity(Intent.createChooser(sharingIntent, header))
+}
+
+fun Context.shareText(text: String, subject: String = "", header: String = "Share") {
+    val sharingIntent = Intent(Intent.ACTION_SEND)
+    sharingIntent.type = "text/plain"
+    if (subject.isNotEmpty()) {
+        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    }
+    sharingIntent.putExtra(Intent.EXTRA_TEXT, text)
+    startActivity(Intent.createChooser(sharingIntent, header))
+}
