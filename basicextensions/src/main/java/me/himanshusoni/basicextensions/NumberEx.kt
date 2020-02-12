f67adc0699be063a7d2648766bc7fb67e12c05af
Himanshu Soni <himanshusoni.me@gmail.com>
2020-02-12 12:32:11 +0530

    number extensions added

--- basicextensions/src/main/java/me/himanshusoni/basicextensions/NumberEx.kt
+++ basicextensions/src/main/java/me/himanshusoni/basicextensions/NumberEx.kt
@@ -21,8 +21,37 @@ fun Number.format(scale: Int, grouping: Boolean = true, forceScale: Boolean = tr
     return numberFormat.format(this)
 }
 
-fun String?.toDoubleOrZero(): Double =
-    if (this?.toDoubleOrNull() == null) 0.toDouble() else this.toDouble()
+fun String?.toDoubleOrDefault(defaultValue: Double): Double =
+    if (this?.toDoubleOrNull() == null) defaultValue else this.toDouble()
 
-fun String?.toIntOrZero(): Int = if (this?.toIntOrNull() == null) 0 else this.toInt()
+fun String?.toDoubleOrZero(): Double = toDoubleOrDefault(0.0)
+
+fun String?.toIntOrDefault(defaultValue: Int): Int =
+    if (this?.toIntOrNull() == null) defaultValue else this.toInt()
+
+fun String?.toIntOrZero(): Int = toIntOrDefault(0)
+
+fun String?.toLongOrDefault(defaultValue: Long): Long =
+    if (this?.toLongOrNull() == null) defaultValue else this.toLong()
+
+fun String?.toLongOrZero(): Long = toLongOrDefault(0)
+
+fun String?.toFloatOrDefault(defaultValue: Float): Float =
+    if (this?.toFloatOrNull() == null) defaultValue else this.toFloat()
+
+fun String?.toFloatOrZero(): Float = toFloatOrDefault(0.0f)
+
 fun Number?.toStringOrBlank(): String = if (this == 0) "" else this.toString()
+
+fun String?.toNonNegativeInt(defaultValue: Int): Int {
+    try {
+        val value = this?.toLong() ?: return defaultValue
+        return when {
+            value > Int.MAX_VALUE -> Int.MAX_VALUE
+            value < 0 -> 0
+            else -> value.toInt()
+        }
+    } catch (_: NumberFormatException) {
+        return defaultValue
+    }
+}
